f99f5fb39d9905b31e63619a5f94be217ae0eed7
Mohd Rustam <rmohd8788@gmail.com>
2025-11-18 13:02:37 +0530

    Added Required Methods in Dao

--- app/src/main/java/com/webmd/newsfeed/data/local/dao/ArticleDao.kt
+++ app/src/main/java/com/webmd/newsfeed/data/local/dao/ArticleDao.kt
@@ -1,7 +1,26 @@
 package com.webmd.newsfeed.data.local.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.webmd.newsfeed.data.local.entity.ArticleEntity
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface ArticleDao {
+    @Query("SELECT * FROM articles ORDER BY lastUpdated DESC")
+    fun getAllArticles(): Flow<List<ArticleEntity>>
+
+    @Query("SELECT * FROM articles WHERE url = :url LIMIT 1")
+    suspend fun getArticleByUrl(url: String): ArticleEntity?
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertArticles(articles: List<ArticleEntity>)
+
+    @Query("DELETE FROM articles")
+    suspend fun deleteAllArticles()
+
+    @Query("SELECT COUNT(*) FROM articles")
+    suspend fun getArticleCount(): Int
 }
